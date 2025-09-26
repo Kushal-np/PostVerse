be4f09656da85049ffc5a8e7410a73332332a27c
Kushal-np <wrckushal@gmail.com>
2025-09-27 01:02:35 +0545

    Initiated the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,15 @@
 import express from 'express';
 const app = express();
+import dotenv from "dotenv";
+import connectDB from './config/db.js';
+dotenv.config();
+const PORT = process.env.PORT ; 
+app.get("/", (req , res)=>{
+    res.send("Hello world");
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running at the ${PORT}`); 
+    connectDB();
+})
+
