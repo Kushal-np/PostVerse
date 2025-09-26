6c185e4c210509f15a7b655434b6f3387fa7628c
Kushal-np <wrckushal@gmail.com>
2025-09-27 01:39:28 +0545

    Done withthe user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String , 
+        unique:true , 
+        required:true , 
+    },
+    email:{
+        type:String , 
+        unique:true , 
+        required : true ,
+    },
+    password:{
+        type:String , 
+        required:true 
+    },
+    role:{
+        type:String, 
+        enum: ["reader" , "writer" , "edition" , "admin"] ,
+        default:"reader" , 
+    } , 
+    name:{
+        type:String , 
+    },
+    bio:{
+        type:String , 
+    },
+    avatarUrl:{
+        type:String ,
+    },
+    social:{
+        twitter:String,
+        github:String ,
+        website:String
+    },
+    settings:{
+        theme:{
+            type:String,
+            enum:["light","dark"],
+            default:"light"
+        },
+        emailNotifications:{
+            type:Boolean,
+            default:true
+
+        }
+    },
+    following:[{
+        type:mongoose.Schema.Types.ObjectId , ref:"User"
+    }],
+    followers:[{
+        type:mongoose.Schema.Types.ObjectId , ref:"User"
+    }],
+    bookmarks:[{
+        type:mongoose.Schema.Types.ObjectId , ref:"Post"
+    }]
+} , {
+    timestamps:true
+})
+
+
+
+
+export default mongoose.model("User" , UserSchema)
