fe9819a175a5817b5465e0edef958dc346b793b3
Kushal-np <wrckushal@gmail.com>
2025-09-28 01:31:08 +0545

    Done with the register controller

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,44 +1,44 @@
-import bcrypt from "bcryptjs"
-import User from "../models/user.model";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 
-export const registerUser = async(req , res) =>{
-    try{
-        const [username , email , password] = req.body; 
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({
-                sucess:false , 
-                message:"User already exists with this email"
-            });
-        }
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-        const salt = bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password,salt);
-
-        const newUser = new User({
-            username , 
-            email , 
-            password:hashedPassword , 
-        });
-
-        await newUser.save();
-
-        res.status(201).json({
-            success:true , 
-            message:"User registered Successfully " , 
-            user:{
-                id:newUser._id , 
-                username:newUser.username , 
-                email: newUser.email , 
-            }
-        });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists with this email",
+      });
     }
-    catch(error){
-        console.log("Register Error:" , error);
-        res.status(500).json({
-            success:false, 
-            message:"Server error during registration"
-        })
-    }
-}
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    console.error("Register Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error during registration",
+    });
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,11 @@ import express from 'express';
 const app = express();
 import dotenv from "dotenv";
 import connectDB from './config/db.js';
+import authRoutes from "./routes/auth.routes.js"
 dotenv.config();
 const PORT = process.env.PORT ; 
-app.get("/", (req , res)=>{
-    res.send("Hello world");
-})
+app.use(express.json())
+app.use("/api/user" , authRoutes)
 
 app.listen(PORT , ()=>{
     console.log(`Server is running at the ${PORT}`); 
