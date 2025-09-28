cf6962ec58abb5e3d8ed3de4b790e8c24a86e314
Kushal-np <wrckushal@gmail.com>
2025-09-28 18:56:22 +0545

    Done with the controllers of the Bookmars routes

--- Backend/controllers/bookmark.controller.js
+++ Backend/controllers/bookmark.controller.js
@@ -1,9 +1,6 @@
 import Bookmark from "../models/Bookmark.model";
 import Post from "../models/post.model";
 
-
-
-
 export const addBookmark = async(req , res)=>{
     try{
         const userId = req.user._id;
@@ -40,6 +37,60 @@ export const addBookmark = async(req , res)=>{
             success:false, 
             message:"Internal server error occured" , 
             error:error.message
+        })
+    }
+}
+
+export const removeBookmark = async(req , res)=>{
+    try{
+        const userId = req.user._id ; 
+        const {postId} = req.params ; 
+        
+        const bookmarks = await Bookmark.findOne({user:userId , post:postId});
+        if(!bookmarks){
+            return res.status(404).json({
+                success:false , 
+                message:"Bookmarks not found",
+            })
+        }
+        await bookmarks.deleteOne();
+        res.status(200).json({
+            success:true , 
+            message:"Bookmark removed"
+        })
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({
+            success:false , 
+            message:"Internal server error"
+        });
+    }
+}
+
+
+export const getAllBookmarks = async(req , res)=>{
+    try{
+        const userId = req.user._id ; 
+        const bookmarks = await Bookmark.find({user:userId})
+        .populate({
+            path:"post",
+            populate:[
+                {path:"author" , select:"username role"},
+                {path:coverImage}
+            ]
+        })
+
+        res.status(200).json({
+            success:true , 
+            bookmarks
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            success:false , 
+            message:"server error"
         })
     }
 }
