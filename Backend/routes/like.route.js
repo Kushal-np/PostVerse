951b0ead4c3732a9c0893dde294bd65efc2c8455
Kushal-np <wrckushal@gmail.com>
2025-09-28 20:05:58 +0545

    Added the route to get the likes on comments

--- Backend/routes/like.route.js
+++ Backend/routes/like.route.js
@@ -1,8 +1,12 @@
 import express from "express"
+import { toggleLikeComment } from "../controllers/comment.controller";
+import { getLikes, toggleLikePost } from "../controllers/like.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 const router = express.Router();
 
+router.post("/toggle" ,authMiddleware ,  toggleLikePost) ; 
+router.get("/:postId" , getLikes) ; 
+router.get("/:commentId " , getCommentsLikes )
 
-
-router.get("/")
 
 export default router ;
